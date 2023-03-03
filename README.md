db45b2cf1dc455c534ffc86c484624c0bbd6a7c5
Daniel Commins <danel.commins@techelevator.com>
2023-03-03 13:56:55 -0800

    initial draft

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pig Latin Translator
+
+Convert the user's text to Pig Latin OR the user's Pig Latin to English!
+
+Follow the instructions in the comments within the `PigLatinTranslator` class.
---
+++ src/main/java/PigLatinTranslator.java
@@ -0,0 +1,34 @@
+public class PigLatinTranslator {
+
+    // 1. Create a main method
+
+
+        // 2. Create a Scanner object
+
+
+        // 3. Ask the user to enter some text to translate into Pig Latin
+        // and save their response into a String variable
+
+
+        // 4. Call the translateEnglishToPigLatin() method from the Translator class
+        // and pass in the user's response. Make sure to save the output into
+        // another String variable. Example:
+        // String pigLatinPhrase = Translator.translateEnglishToPigLatin(userText);
+
+
+        // 5. Print out the translated text to the user
+
+
+        // 6. Call the speak() method from the Translator class to have your computer
+        // speak the Pig Latin phrase! Example:
+        // Translator.speak(pigLatinPhrase);
+
+
+        /*
+         * BONUS: Can figure out how to add functionality to accept a Pig-Latin
+         *        phrase from the user and translate it to English??
+         *        You may find the following method useful:
+         *        Translator.translatePigLatinToEnglish(pigLatinPhrase)
+         */
+
+}
